aa6f2cfb0bb603db2b01f4297b17803a4be522f2
Austin <aj_viens_deruisseau@yahoo.com>
2018-04-20 21:59:18 -0400

    Camera API added and taking pictures

--- app/src/main/java/com/personaltrainer/android/personaltrainer/newCustomer.java
+++ app/src/main/java/com/personaltrainer/android/personaltrainer/newCustomer.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,14 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 import android.widget.ListView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Austin on 3/17/2018.
  */
@@ -38,11 +47,11 @@ public class newCustomer extends Activity implements android.view.View.OnClickLi
         setContentView(R.layout.new_customer);
 
         // Code is commented for AVD usage
-//        frameLayout = findViewById(R.id.frameLayout1);
-//
-//        camera = Camera.open();
-//        showCamera = new CameraView(this, camera);
-//        frameLayout.addView(showCamera);
+        frameLayout = findViewById(R.id.frameLayout1);
+
+        camera = Camera.open();
+        showCamera = new CameraView(this, camera);
+        frameLayout.addView(showCamera);
 
         btnSave = findViewById(R.id.btnSave);
         btnDelete = findViewById(R.id.btnDelete);
@@ -73,19 +82,49 @@ public class newCustomer extends Activity implements android.view.View.OnClickLi
 
     }
     // Code is commented for AVD usage
-//
-//    Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
-//        @Override
-//        public void onPictureTaken(byte[] data, Camera camera) {
-//
-//        }
-//    };
-//
-//    public void captureImage(View v) {
-//        if (camera != null) {
-//            camera.takePicture(null, null, mPictureCallback);
-//        }
-//    }
+
+    Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File picture_file = getOutputMediaFile();
+
+            if(picture_file == null) {
+                return;
+            } else {
+                try {
+                    FileOutputStream fos = new FileOutputStream(picture_file);
+                    fos.write(data);
+                    fos.close();
+
+                    camera.startPreview();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    private File getOutputMediaFile() {
+        String state = Environment.getExternalStorageState();
+        if(!state.equals(Environment.MEDIA_MOUNTED))
+        {
+            return null;
+        } else {
+            File folder_gui = new File(Environment.getExternalStorageDirectory() + File.separator + "GUI");
+            if(!folder_gui.exists()) {
+                folder_gui.mkdirs();
+            }
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            File outputFile = new File(folder_gui, timestamp + ".png");
+            return outputFile;
+        }
+    }
+
+    public void captureImage(View v) {
+        if (camera != null) {
+            camera.takePicture(null, null, mPictureCallback);
+        }
+    }
 
 
     public void onClick(View view) {
